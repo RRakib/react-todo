b4c63bd3e4a5ee34b3f0042d81a122981348f627
Rakib Uddin <16103030@iubat.edu>
2019-01-11 17:11:00 +0600

    Created TodoItems

--- src/App.js
+++ src/App.js
@@ -3,11 +3,29 @@ import Todos from "./Components/Todos"
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+         todo : [
+           {
+             id: 1,
+             title : "Wakeup at 8am",
+             completed: false
+           },
+           {
+             id: 2,
+             title : "Sit for code",
+             completed: false
+           }
+         ]
+
+    }
+  }
   render() {
     return (
       <div className="App">
         <h1>Hello I am Working</h1>
-        <Todos />
+        <Todos todos={this.state.todo}/>
       </div>
     );
   }
--- src/Components/Todos.js
+++ src/Components/Todos.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
-
+import TodoItems from "./TodoItems"
 class Todos extends Component {
   render() {
+      let todos = this.props.todos.map((items) => {
+          return(
+              <TodoItems key={items.id} titles = {items.title} />
+          )
+      })
     return (
       <div>
         <h1>Todos</h1>
+        {todos}
       </div>
     )
   }
