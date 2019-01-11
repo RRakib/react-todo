f7735bbe3bd7ad7f1e5d547dd130de38193dcf9c
Rakib Uddin <16103030@iubat.edu>
2019-01-11 14:23:25 +0600

    Create Todo.js And Import It On App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Todos from "./Components/Todos"
 import './App.css';
 
 class App extends Component {
@@ -6,6 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hello I am Working</h1>
+        <Todos />
       </div>
     );
   }
